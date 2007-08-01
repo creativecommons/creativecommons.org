5bdc7f3fe55f5030fa8d94370b1d5ad5663b1bc8
nyergler <nyergler@aec4f576-4910-0410-beeb-ecb0427469e7>
2007-08-01 23:53:26 +0000

    * Return 404 w/invalid version * Implemented attribute retrieval for deed views.
    git-svn-id: svn+ssh://code.creativecommons.org/svnroot/cc.engine/trunk@6523 aec4f576-4910-0410-beeb-ecb0427469e7

--- python_env/src/cc.engine/cc/engine/licenses.py
+++ python_env/src/cc.engine/cc/engine/licenses.py
@@ -1,10 +1,12 @@
 import grok
 from zope.interface import implements
 from zope.publisher.interfaces import NotFound
+from zope.i18n import translate
+
 import cc.license
+from cc.license.exceptions import LicenseException
 
 from cc.engine import interfaces
-
 
 class BrowserLicense(grok.Model):
     implements(interfaces.ILicense)
@@ -22,12 +24,63 @@ class BrowserLicense(grok.Model):
     def license(self):
         """Return the cc.license.License object selected."""
 
-        version = jurisdiction = None
+        # decode the version and jurisdiction
+        if len(self.pieces) > 2:
+            version, jurisdiction = self.pieces[1:3]
+        elif len(self.pieces) > 1:
+            version, jurisdiction = self.pieces[1], None
         
+        # XXX cache me!
         return cc.license.LicenseFactory().by_license_code(self.pieces[0],
                                                            version,
                                                            jurisdiction)
 
+    @property
+    def conditions(self):
+        """Return a sequence of mappings defining the conditions defined by
+        this license."""
+
+        attrs = []
+
+        for lic in self.license.code.split('-'):
+
+            # Go through the chars and build up the HTML and such
+            char_title = translate ('char.%s_title' % lic,
+                                    domain='icommons')
+            char_brief = translate ('char.%s_brief' % lic,
+                                    domain='icommons')
+
+            icon_name = lic
+            predicate = 'cc:requires'
+            object = 'http://creativecommons.org/ns#Attribution'
+
+            if lic == 'nc':
+              predicate = 'cc:prohibits'
+              object = 'http://creativecommons.org/ns#CommercialUse'
+              if self.license.jurisdiction == 'jp':
+                 icon_name = '%s-jp' % icon_name
+              elif self.license.jurisdiction in ('fr', 'es', 'nl', 'at', 'fi', 'be', 'it'):
+                 icon_name = '%s-eu' % icon_name
+            elif lic == 'sa':
+              object = 'http://creativecommons.org/ns#ShareAlike'
+              if self.license.version == 3.0 and self.license.code == 'by-sa':
+                char_brief = translate ('char.sa_bysa30_brief',
+                                        domain='icommons')
+            elif lic == 'nd':
+              predicate = ''
+              object = ''
+
+            attrs.append({'char_title':char_title,
+                          'char_brief':char_brief,
+                          'icon_name':icon_name,
+                          'char_code':lic,
+                          'predicate':predicate,
+                          'object':object,
+                     })
+
+        # XXX cache me!
+        return attrs
+        
     def traverse(self, name):
 
         if len(self.pieces) > 3:
@@ -44,7 +97,22 @@ class LicenseDeed(grok.View):
     grok.context(BrowserLicense)
     grok.name('index')
     grok.template('deed')
-    
+
+    def update(self):
+        """Prepare to render the deed."""
+
+        # redirect if we don't have a trailing slash
+        if self.request['REQUEST_URI'][-1] != '/':
+            return self.request.response.redirect('%s/' % self.request['REQUEST_URI'])
+
+        # make sure we've traversed to a valid license version
+        try:
+            self.context.license
+        except LicenseException, e:
+            raise NotFound(self.context, self.request['REQUEST_URI'],
+                           self.request)
+
+        
     @property
     def is_rtl(self):
 
