3aa74948f17025ce1cb910be034215e5136d5aac
nyergler <nyergler@aec4f576-4910-0410-beeb-ecb0427469e7>
2007-08-01 22:06:29 +0000

    Look @ the querystring for determining the preferred language.
    git-svn-id: svn+ssh://code.creativecommons.org/svnroot/cc.engine/trunk@6516 aec4f576-4910-0410-beeb-ecb0427469e7

---
+++ python_env/src/cc.engine/cc/engine/i18n.py
@@ -0,0 +1,34 @@
+"""Internalization support for cc.engine."""
+
+from zope.interface import implements
+from zope.i18n.interfaces import IUserPreferredLanguages
+from zope.publisher.browser import BrowserLanguages
+
+class PreferredLanguages(object):
+    """Custom language selector: looks for a language setting in the
+    following order:
+
+    * ?lang=xx querystring
+    * ?language=xx querystring
+    * cc_org_lang cookie
+    * HTTP_ACCEPT header
+    """
+    implements(IUserPreferredLanguages)
+
+    def __init__(self, request):
+        self.request = request
+
+    def getPreferredLanguages(self):
+
+        # check for the query string
+        if self.request.get('lang', False):
+            return [self.request['lang'], 'en']
+
+        if self.request.get('language', False):
+            return [self.request['language'], 'en']
+        
+        # XXX look for the cookie
+        
+        # fall back to default selection (HTTP_ACCEPT_LANGUAGE)
+        return BrowserLanguages(self.request).getPreferredLanguages()
+
