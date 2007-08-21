c1882a6dffa0998a94875638de8b6156e96d34b6
nyergler <nyergler@aec4f576-4910-0410-beeb-ecb0427469e7>
2007-08-21 16:26:40 +0000

    Initialize the ZODB when run the application.
    git-svn-id: svn+ssh://code.creativecommons.org/svnroot/cc.engine/trunk@6821 aec4f576-4910-0410-beeb-ecb0427469e7

--- python_env/src/cc.engine/cc/engine/__init__.py
+++ python_env/src/cc.engine/cc/engine/__init__.py
@@ -1,2 +1,32 @@
+"""
+cc.engine
+=========
+
+See README.txt.
+
+Copyright 2007 Nathan R. Yergler, Creative Commons <nathan@creativecommons.org>
+"""
+
+from zope.component import adapter
+from zope.app.appsetup.interfaces import IDatabaseOpenedWithRootEvent
+from zope.app.publication.zopepublication import ZopePublication
+
+import cc.engine.chooser
+import cc.engine.licenses.catalog
+
+@adapter(IDatabaseOpenedWithRootEvent)
+def init(event):
+    """Subscribe to database open events to initialize our objects."""
+
+    db_root = event.database.open().root()[ZopePublication.root_name]
+                                           
+    # check for the license chooser
+    if db_root.get('license', None) is None:
+        db_root['license'] = cc.engine.chooser.LicenseEngine()
+
+    # check for the deed catalog
+    if db_root.get('licenses', None) is None:
+        db_root['licenses'] = cc.engine.licenses.catalog.LicenseCatalog()
+    
 
 
