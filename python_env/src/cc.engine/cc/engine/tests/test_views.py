7b7527491830dabc8a1f546b36f4b646a00d9cdb
Christopher Allan Webber <cwebber@dustycloud.org>
2009-11-10 12:59:34 -0600

    test for licenses_view

--- python_env/src/cc.engine/cc/engine/tests/test_views.py
+++ python_env/src/cc.engine/cc/engine/tests/test_views.py
@@ -9,7 +9,27 @@ from cc.engine import views
 util._activate_zpt_testing()
 
 
+### ---------------
+### routing testing
+### ---------------
+
+
+### ------------
+### view testing
+### ------------
+
 def test_root_view():
     response = views.root_view(Request.blank('/'))
     assert response.unicode_body == 'This is the root'
 
+
+def test_licenses_view():
+    request = Request('/licenses/')
+    response = views.licenses_view(request)
+    namespace = util.ZPT_TEST_TEMPLATES.pop(
+        util.full_zpt_filename('catalog_pages/licenses-index.pt'))
+    namespace['request'] == request
+
+
+def test_license_deed_view():
+    pass
