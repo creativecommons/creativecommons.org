6a8d0d4af6fc2e675c947306522fc0b9f958d801
Christopher Allan Webber <cwebber@dustycloud.org>
2009-11-10 19:05:11 -0600

    BaseViewTests class and assoc framework, and an RDF view test

--- python_env/src/cc.engine/cc/engine/tests/test_views.py
+++ python_env/src/cc.engine/cc/engine/tests/test_views.py
@@ -1,10 +1,12 @@
+import pkg_resources
 import lxml
 
 from webob import Request
 
+from cc.engine.app import load_controller
 from cc.engine import util
 from cc.engine import views
-
+import cc.license
 
 util._activate_zpt_testing()
 
@@ -12,6 +14,9 @@ util._activate_zpt_testing()
 ### ---------------
 ### routing testing
 ### ---------------
+
+def test_routing():
+    pass
 
 
 ### ------------
@@ -24,12 +29,52 @@ def test_root_view():
 
 
 def test_licenses_view():
-    request = Request('/licenses/')
+    request = Request.blank('/licenses/')
     response = views.licenses_view(request)
     namespace = util.ZPT_TEST_TEMPLATES.pop(
         util.full_zpt_filename('catalog_pages/licenses-index.pt'))
     namespace['request'] == request
 
 
-def test_license_deed_view():
+def _lc_tester():
+    pass
+
+
+#    expected_license = cc.license.by_code(
+
+
+### Basic view testing class
+
+class BaseViewTests(object):
+    def setUp(self):
+        self.request = Request.blank(self.url)
+        self.request.matchdict = self.matchdict
+        self.controller = load_controller(self.matchdict['controller'])
+        self.response = self.controller(self.request)
+
+
+class BaseTestLicenseRdfView(BaseViewTests):
+    def _read_rdf_file_contents(self):
+        return file(self.rdf_file).read()
+
+    def test_has_rdf(self):
+        rdf_contents = util.unicode_cleaner(self._read_rdf_file_contents())
+        assert rdf_contents == self.response.unicode_body
+        
+    def test_headers(self):
+        expected_header = 'application/rdf+xml; charset=UTF-8'
+        assert self.response.headers['Content-Type'] == expected_header
+
+
+class TestBySaRDFView(BaseTestLicenseRdfView):
+    url = '/licenses/by-sa/2.0/rdf'
+    matchdict = {
+        'code': 'by-sa',
+        'version': '2.0',
+        'controller': 'cc.engine.views:license_rdf_view'}
+    rdf_file = pkg_resources.resource_filename(
+        'cc.licenserdf', 'licenses/creativecommons.org_licenses_by-sa_2.0_.rdf')
+
+
+def test_license_deeds():
     pass
