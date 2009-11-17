a8b606a289b6d43e03803de1dfd153a89f9bf858
Christopher Allan Webber <cwebber@dustycloud.org>
2009-11-17 14:35:37 -0600

    Adding cc0 to the licenses/routing.py, also naming the extended routes to trim things down

--- python_env/src/cc.engine/cc/engine/routing.py
+++ python_env/src/cc.engine/cc/engine/routing.py
@@ -7,16 +7,5 @@ mapping.minimization = False
 mapping.connect(
     "index", "/", controller="cc.engine.views:root_view")
 
-mapping.extend(licenses_routing.licenses_routes)
-
-# CC0
-mapping.connect(
-    "cc0_deed",
-    "/publicdomain/zero/{version}/",
-    code='CC0',
-    controller="cc.engine.licenses.views:license_deed_view")
-mapping.connect(
-    "cc0_deed_lang",
-    "/publicdomain/zero/{version}/deed.{target_lang}",
-    code='CC0',
-    controller="cc.engine.licenses.views:license_deed_view")
+mapping.extend(licenses_routing.licenses_routes, '/licenses')
+mapping.extend(licenses_routing.cc0_routes, '/publicdomain/zero')
