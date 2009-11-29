aefe5ed68c6010e70155e1ec2d9135b80f4f4ac1
Christopher Allan Webber <cwebber@dustycloud.org>
2009-11-28 22:30:20 -0600

    Separated out some of the common context stuff and began to make the results function

--- python_env/src/cc.engine/cc/engine/chooser/views.py
+++ python_env/src/cc.engine/cc/engine/chooser/views.py
@@ -4,6 +4,20 @@ from cc.engine import util
 from cc.i18npkg import ccorg_i18n_setup
 
 
+def _base_context(request):
+    context = {
+        'request': request,
+        'target_lang': (
+            request.matchdict.get('target_lang')
+            or request.accept_language.best_matches()[0]),
+        'active_languages': util.active_languages(),
+        'selected_jurisdiction': util.get_selected_jurisdiction(request),
+        }
+    
+    context.update(util.rtl_context_stuff(request))
+    return context
+
+
 def chooser_view(request):
     template = util.get_zpt_template('chooser_pages/index.pt')
     engine_template = util.get_zpt_template(
@@ -13,24 +27,26 @@ def chooser_view(request):
     support_template = util.get_zpt_template(
         'macros_templates/support.pt')
 
-    jurisdiction = util.get_selected_jurisdiction(request)
-
     available_jurisdiction_codes = [
         j.code for j in util.get_selector_jurisdictions('standard')]
     
-    target_lang = (
-        request.matchdict.get('target_lang')
-        or request.accept_language.best_matches()[0])
-    active_languages = util.active_languages()
+    context = _base_context(request)
+    context.update(
+        {'engine_template': engine_template,
+         'metadata_template': metadata_template,
+         'support_template': support_template,
+         'available_jurisdiction_codes': available_jurisdiction_codes})
 
-    context = {'request': request,
-               'engine_template': engine_template,
-               'metadata_template': metadata_template,
-               'support_template': support_template,
-               'selected_jurisdiction': jurisdiction,
-               'available_jurisdiction_codes': available_jurisdiction_codes,
-               'target_lang': target_lang,
-               'active_languages': active_languages}
-    context.update(util.rtl_context_stuff(request))
+    return Response(template.pt_render(context))
+
+
+def choose_results_view(request):
+    template = util.get_zpt_template('chooser_pages/results.pt')
+    engine_template = util.get_zpt_template(
+        'macros_templates/engine.pt')
+
+    context = _base_context(request)
+    context.update(
+        {'engine_template': engine_template})
 
     return Response(template.pt_render(context))
