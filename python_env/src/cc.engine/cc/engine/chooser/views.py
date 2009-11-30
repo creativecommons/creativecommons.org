7622baca7aa0bc0055029d76d1d42affc61b6d2b
Christopher Allan Webber <cwebber@dustycloud.org>
2009-11-30 15:00:29 -0600

    partway to adding _issue_license

--- python_env/src/cc.engine/cc/engine/chooser/views.py
+++ python_env/src/cc.engine/cc/engine/chooser/views.py
@@ -18,6 +18,67 @@ def _base_context(request):
     return context
 
 
+def _issue_license(request):
+    """Extract the license engine fields from the request and return a
+    License object."""
+
+    request_form = request.GET or request.POST
+
+    jurisdiction = ''
+    code = ''
+
+    license_class = 'standard'
+    answers = {}
+
+    if request_form.has_key('pd') or \
+            request_form.has_key('publicdomain') or \
+            request_form.get('license_code', None)  == 'publicdomain':
+       # this is public domain
+       license_class = 'publicdomain'
+
+    # check for license_code
+    elif request_form.has_key('license_code'):
+       jurisdiction = request_form.get(
+           'jurisdiction',
+           request_form.get('field_jurisdiction', ''))
+
+       license_class, answers = cc.license.support.expandLicenseCode(
+           request_form.get('license_code'),
+           jurisdiction = jurisdiction,
+           version = request_form.get('version', None))
+
+    # check for license_url
+    elif request_form.has_key('license_url'):
+        # work backwards, generating the answers from the license code
+        code, jurisdiction, version = cc.license.support.expand_license_uri(
+            request_form['license_url'])
+        license_class, answers = cc.license.support.expandLicenseCode(
+            code, jurisdiction)
+
+        answers['version'] = version
+
+    else:
+       jurisdiction = request_form.get(
+           'field_jurisdiction', jurisdiction)
+
+       answers.update(
+           {'jurisdiction': request_form.get(
+                   'field_jurisdiction', jurisdiction),
+            'commercial': request_form.get('field_commercial', ''),
+            'derivatives': request_form.get('field_derivatives', '')})
+
+       if request_form.get('version', False):
+           answers['version'] = request_form['version']
+
+    # add the work to the answers block
+    answers.update(self._work_info(request))
+
+    # return the license object
+    return cc.license.LicenseFactory().get_class(license_class).issue(
+        **answers)
+
+
+
 def chooser_view(request):
     template = util.get_zpt_template('chooser_pages/index.pt')
     engine_template = util.get_zpt_template(
