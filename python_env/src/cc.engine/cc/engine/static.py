544294ace83880067f960ccfb2958c39cef78a30
Christopher Allan Webber <cwebber@dustycloud.org>
2010-01-05 00:10:28 -0600

    Making slashes in static resource url direction work better

--- python_env/src/cc.engine/cc/engine/static.py
+++ python_env/src/cc.engine/cc/engine/static.py
@@ -24,25 +24,29 @@ class LocalStaticDirect(StaticDirect):
     except for development!
     """
     def get(self, request, filepath):
-        return request.urlgen('staticserve', filename=filepath)
+        return request.urlgen('staticserve', filename=filepath.lstrip('/'))
 
 
 class RemoteStaticDirect(StaticDirect):
     def __init__(self, remotepath):
+        StaticDirect.__init__(self)
         self.remotepath = remotepath
 
     def get(self, request, filepath):
-        return urlparse.urljoin(self.remotepath, filepath)
+        return '%s/%s' % (
+            self.remotepath, rest.lstrip('/'))
 
 
 class MultiRemoteStaticDirect(StaticDirect):
     """
     """
     def __init__(self, remotepaths):
+        StaticDirect.__init__(self)
         self.remotepaths = remotepaths
 
     def get(self, request, filepath):
         section, rest = filepath.strip('/').split('/', 1)
 
-        return urlparse.urljoin(
-            self.remotepaths[section], rest)
+        return '%s/%s' % (
+            self.remotepaths[section].rstrip('/'),
+            rest.lstrip('/'))
