7b2d10d6c871f2c33af6c39241f51e91afa50e28
Christopher Allan Webber <cwebber@dustycloud.org>
2010-01-06 14:33:10 -0600

    Restoring the old really simple root_view

--- python_env/src/cc.engine/cc/engine/views.py
+++ python_env/src/cc.engine/cc/engine/views.py
@@ -6,6 +6,4 @@ from cc.engine import util
 
 
 def root_view(request):
-    return Response("/foo/what is at %s, /blah/bar is at %s" % (
-            request.staticdirect('/foo/what/'),
-            request.staticdirect('/blah/bar/')))
+    return Response("This is the root")
