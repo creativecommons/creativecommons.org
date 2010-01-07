79bf2280649a43eee8634526fa6163bc3cc560cd
Christopher Allan Webber <cwebber@dustycloud.org>
2010-01-07 10:46:07 -0600

    Starting to add staticdirect tests

---
+++ python_env/src/cc.engine/cc/engine/tests/test_staticdirect.py
@@ -0,0 +1,17 @@
+from cc.engine import staticdirect
+
+
+def test_remote_staticdirect():
+    assert 0
+
+
+def test_multi_remote_staticdirect():
+    assert 0
+
+
+def test_request_dot_staticdirect():
+    assert 0
+
+
+def test_static_app_factory():
+    assert 0
