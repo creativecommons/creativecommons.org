afca3a90259d7b07a5db6b1e49b38468c14d18ac
Christopher Allan Webber <cwebber@dustycloud.org>
2010-01-07 11:17:15 -0600

    Tests for test_remote_staticdirect and test_multi_remote_staticdirect

--- python_env/src/cc.engine/cc/engine/tests/test_staticdirect.py
+++ python_env/src/cc.engine/cc/engine/tests/test_staticdirect.py
@@ -2,11 +2,21 @@ from cc.engine import staticdirect
 
 
 def test_remote_staticdirect():
-    assert 0
+    sd = staticdirect.RemoteStaticDirect('/statik')
+    assert sd('/foo/') == '/statik/foo/'
+    assert sd('/foo/bar/baz') == '/statik/foo/bar/baz'
+    assert sd('blah') == '/statik/blah'
 
 
 def test_multi_remote_staticdirect():
-    assert 0
+    sd = staticdirect.MultiRemoteStaticDirect(
+        {'images': 'http://img.example.org/images/',
+         'js': '/statik/js/',
+         'css': '/statik/stylesheets/'})
+    assert sd('/images/intarwebs.jpg') == \
+        'http://img.example.org/images/intarwebs.jpg'
+    assert sd('/js/joovascreept.js') == '/statik/js/joovascreept.js'
+    assert sd('/css/stylish.css') == '/statik/stylesheets/stylish.css'
 
 
 def test_request_dot_staticdirect():
