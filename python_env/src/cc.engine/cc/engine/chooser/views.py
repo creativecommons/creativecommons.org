d15b8af6c4525adb70d5040b771975b5ecbc5f30
Christopher Allan Webber <cwebber@dustycloud.org>
2010-05-07 17:35:29 -0500

    I guess we also put the charset here?.. I'm not sure..

--- python_env/src/cc.engine/cc/engine/chooser/views.py
+++ python_env/src/cc.engine/cc/engine/chooser/views.py
@@ -330,7 +330,7 @@ def choose_xmp_view(request):
 
     return Response(
         xmp_data,
-        content_type='application/xmp',
+        content_type='application/xmp; charset=UTF-8',
         charset='UTF-8',
         content_disposition='attachment; filename="CC_%s.xmp' % (
             license.title().strip().replace(' ', '_')))
