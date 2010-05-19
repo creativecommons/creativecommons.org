58210f9d3a3d3376762c80fc34201f0cd9699046
Christopher Allan Webber <cwebber@dustycloud.org>
2010-05-19 11:24:37 -0500

    Added a scraper for launched_jurisdictions and starting to work on test_jurisdiction_dropdown_contains_jurisdictions

---
+++ python_env/src/cc.engine/cc/engine/tests/test_international.py
@@ -0,0 +1,44 @@
+from lxml import html
+import webtest
+
+from cc.engine import app, staticdirect
+
+
+TESTAPP = webtest.TestApp(
+    app.CCEngineApp(
+        staticdirect.RemoteStaticDirect('/static/'), {}))
+
+
+def scraped_launched_jurisdictions():
+    international_dom = html.parse('http://creativecommons.org/international/')
+    completed_block = international_dom.xpath(
+        "id('blocks')/div/div[@class='icontainer']")[0]
+    jurisdictions = [
+        a.attrib['href'].strip('/').split('/')[1]
+        for a in completed_block.xpath("div/a")]
+
+    return jurisdictions
+    
+
+def test_scraped_launched_jurisdictions():
+    """
+    Test to make sure that scraped_launched_jurisdictions is still
+    pulling stuff.
+
+    Yes, testing a test utility.
+    """
+    jurisdictions = scraped_launched_jurisdictions()
+
+    # A few relatively safe jurisdictions just to be sure this works
+    assert 'us' in jurisdictions
+    assert 'fr' in jurisdictions
+
+
+def test_jurisdiction_dropdown_contains_jurisdictions():
+    """
+    Make sure that all the jurisdictions listed in
+    scraped_launched_jurisdictions show up in the jurisdictions
+    dropdown.
+    """
+    response = TESTAPP.get('/choose/')
+    html.fromstring(response.body)
