f43001f58e22466e29a76d4fab370521060a54a5
Christopher Allan Webber <cwebber@dustycloud.org>
2010-06-17 10:48:13 -0500

    New version number

--- python_env/src/cc.engine/setup.py
+++ python_env/src/cc.engine/setup.py
@@ -25,7 +25,7 @@ import sys
 
 setup(
     name = "cc.engine",
-    version = "10.0.8",
+    version = "10.0.9",
     namespace_packages = ['cc',],
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     
