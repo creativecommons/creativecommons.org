0a0178de289d41e4a434d1b32796d7cea5801731
Christopher Allan Webber <cwebber@dustycloud.org>
2010-12-09 12:31:48 -0600

    [L]GPL deeds and RDF should redirect to gnu.org

--- python_env/src/cc.engine/cc/engine/licenses/views.py
+++ python_env/src/cc.engine/cc/engine/licenses/views.py
@@ -191,3 +191,33 @@ def license_legalcode_redirect(request):
     elif request.matchdict['code'] == 'BSD':
         return exc.HTTPMovedPermanently(
             location='http://opensource.org/licenses/bsd-license.php')
+
+
+# [L]GPL redirects
+def gpl_redirect(request):
+    """
+    Move GPL 2.0 deeds to gnu.org's domain
+    """
+    return exc.HTTPMovedPermanently(
+        location='http://www.gnu.org/licenses/gpl-2.0.html')
+
+def gpl_rdf_redirect(request):
+    """
+    Move GPL 2.0 rdf to gnu.org's domain
+    """
+    return exc.HTTPMovedPermanently(
+        location='http://www.gnu.org/licenses/gpl-2.0.rdf')
+
+def lgpl_redirect(request):
+    """
+    Move LGPL 2.1 deeds to gnu.org's domain
+    """
+    return exc.HTTPMovedPermanently(
+        location='http://www.gnu.org/licenses/lgpl-2.1.html')
+
+def lgpl_rdf_redirect(request):
+    """
+    Move LGPL 2.1 rdf to gnu.org's domain
+    """
+    return exc.HTTPMovedPermanently(
+        location='http://www.gnu.org/licenses/lgpl-2.1.rdf')
