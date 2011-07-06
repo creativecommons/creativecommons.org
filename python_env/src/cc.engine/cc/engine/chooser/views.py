439f62917541aadc1a4a3c5f9655650c8f74e323
Christopher Allan Webber <cwebber@dustycloud.org>
2011-07-06 14:47:33 -0500

    CC0 partner page... also with Jinja2!
    It's like a Jinja2 party in here!

--- python_env/src/cc.engine/cc/engine/chooser/views.py
+++ python_env/src/cc.engine/cc/engine/chooser/views.py
@@ -651,9 +651,6 @@ def cc0_partner(request):
     """
     target_lang = util.get_target_lang_from_request(request)
 
-    template = util.get_zpt_template(
-        'chooser_pages/zero/partner.pt', target_lang)
-
     request_form = request.GET or request.POST
 
     cc0_license = cc.license.by_code('CC0')
@@ -663,16 +660,16 @@ def cc0_partner(request):
 
     context = _base_context(request, target_lang)
     context.update(
-        {'partner_template': util.get_zpt_template(
-                'macros_templates/partner.pt', target_lang),
-         'request_form': request_form,
+        {'request_form': request_form,
          'get_params': get_params,
          'exit_url': _generate_exit_url(
                 request_form.get('exit_url', ''),
                 request_form.get('referrer', ''),
                 cc0_license)})
 
-    return Response(template.pt_render(context))
+    return util.render_to_response(
+        request, target_lang,
+        'chooser_pages/zero/partner.html', context)
 
 
 # publicdomain-direct now redirects to CC0!
