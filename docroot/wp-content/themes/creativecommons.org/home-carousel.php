a5852bed73c5a5ffa755075ea0f1e166b368ddeb
cxadams <cxadams@committer.creativecommons.org>
2011-09-10 12:12:00 +0000

    update carousel links

--- docroot/wp-content/themes/creativecommons.org/home-carousel.php
+++ docroot/wp-content/themes/creativecommons.org/home-carousel.php
@@ -30,14 +30,14 @@
 												<p>For three days from 16-18 September 2011, the Creative Commons Global Summit, <strong>Powering an Open Future</strong>, will bring together the CC community in Warsaw, Poland</p>
 
 												<p><strong>Registration for the Summit is open!</strong> You can register now.<p>
-												<div class="bucket-follow"><a class="primary btn" href="http://wiki.creativecommons.org/Global_Summit_2011?utm_campaign=newsletter_1108&amp;utm_medium=blog&amp;utm_source=newsletter">Register now</a></div>
+												<div class="bucket-follow"><a class="primary btn" href="http://wiki.creativecommons.org/Global_Summit_2011">Learn more</a></div>
 											</div> <!--! end of "content" -->
 										</div> <!--! end of "five columns alpha" -->
 										<div class="eleven columns omega">
 											<div class="film">
-												<a href="http://wiki.creativecommons.org/Global_Summit_2011?utm_campaign=newsletter_1108&amp;utm_medium=blog&amp;utm_source=newsletter"><img src="<?php bloginfo('stylesheet_directory'); ?>/img/ccline.png" alt="Creative Commons Global Summit 2011" /></a>
+												<a href="http://wiki.creativecommons.org/Global_Summit_2011"><img src="<?php bloginfo('stylesheet_directory'); ?>/img/ccline.png" alt="Creative Commons Global Summit 2011" /></a>
 												<div class="caption">
-													<p>The CC Global Summit is taking place 16–17 September 2011 in Warsaw Poland. <a href="http://wiki.creativecommons.org/Global_Summit_2011?utm_campaign=newsletter_1108&amp;utm_medium=blog&amp;utm_source=newsletter">Register now!</a></p>
+													<p>The CC Global Summit is taking place 16–17 September 2011 in Warsaw Poland. <a href="http://wiki.creativecommons.org/Global_Summit_2011">Learn more!</a></p>
 												</div>
 											</div><!--! end of "film" -->
 										</div><!--! end of "eleven columns omega" -->
@@ -50,14 +50,14 @@
 												<p>The stories in <strong>The Power of Open</strong> demonstrate the breadth and creativity of the individuals and organizations using CC.</p>
 												<p>Learn more by <strong>reading the book</strong>!</p>
 
-												<div class="bucket-follow"><a class="primary btn" href="<?php bloginfo('home');?>/weblog/entry/27742">Read the Book</a></div>
+												<div class="bucket-follow"><a class="primary btn" href="http://thepowerofopen.org/">Read the Book</a></div>
 											</div> <!--! end of "content" -->
 										</div> <!--! end of "five columns alpha" -->
 										<div class="eleven columns omega">
 											<div class="film">
 											<a href="<?php bloginfo('home');?>/weblog/entry/27742"><img src="<?php bloginfo('stylesheet_directory'); ?>/img/poo.jpg" alt="The Power of Open" /></a>
 												<div class="caption">
-													<p>The Power of Open: Stories of creators sharing knowledge, art, & data using Creative Commons. <a href="<?php bloginfo('home');?>/weblog/entry/27742">Read it now</a>.</p>
+													<p>The Power of Open: Stories of creators sharing knowledge, art, & data using Creative Commons. <a href="http://thepowerofopen.org/">Read it now</a>.</p>
 												</div>
 											</div><!--! end of "film" -->
 										</div><!--! end of "eleven columns omega" -->
