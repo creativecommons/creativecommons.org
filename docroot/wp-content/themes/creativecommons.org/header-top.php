c624da42d3260802bd85f6fc2aa7a2f67edeca4a
rejon <rejon@committer.creativecommons.org>
2011-09-16 14:49:41 +0000

    removed

--- docroot/wp-content/themes/creativecommons.org/header-top.php
+++ docroot/wp-content/themes/creativecommons.org/header-top.php
@@ -9,15 +9,9 @@
 <!-- Consider adding a manifest.appcache: h5bp.com/d/Offline -->
 <!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 <head profile="http://gmpg.org/xfn/11">
-	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" charset="<?php bloginfo('charset'); ?>" />
+    <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" charset="<?php bloginfo('charset'); ?>" />
     <meta name="author" content="fabricatorz" />
-
-	<!-- Use the .htaccess and remove these lines to avoid edge case issues.
-			 More info: h5bp.com/b/378 -->
-	<!--<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">-->
-
-	<!--<meta name="viewport" content="width=device-width,initial-scale=1">-->
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0" />
 
 	<link href="<?php bloginfo('stylesheet_directory'); ?>/favicon.ico" title="Icon" type="image/x-icon" rel="icon" />
 	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php bloginfo('stylesheet_directory'); ?>/apple-touch-icon-114x114-precomposed.png">
@@ -28,7 +22,6 @@
 
     <script src="<?php bloginfo('stylesheet_directory'); ?>/js/libs/modernizr-2.0.6.min.js"></script>
 
-    <!-- why not from the database? -->
     <?php if (is_front_page() || is_404()) {?>
     <title>Creative Commons</title>
     <?php } else if (is_search()) { ?>
@@ -37,7 +30,6 @@
     <title><?php wp_title(''); ?> - Creative Commons</title>
     <?php }?>
 
-    <!-- why not from the database? -->
     <meta name="keywords" content="creative commons, commons, free culture, open source, attribution, non-commercial, share-alike, no derivatives, lessig" />
     <meta name="description" content="Creative Commons licenses provide a flexible range of protections and freedoms for authors, artists, and educators. <?php if (is_single()) echo cc_post_excerpt() ?>" />
 
@@ -52,7 +44,6 @@
     <?php } else if (is_tag()) { ?>
     <link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php echo get_tag_link($tag_id); ?>/feed/rss" />
     <?php } ?>
-
 
 	<?php wp_head(); ?>
 </head>
