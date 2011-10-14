1c67905424b9a4b04899e3a25bc710d801879072
nkinkade <nkinkade@committer.creativecommons.org>
2011-10-14 21:50:57 +0000

    Added Analytics tracking to campaign header bar

--- docroot/wp-content/themes/creativecommons.org/banner-donate.php
+++ docroot/wp-content/themes/creativecommons.org/banner-donate.php
@@ -1,12 +1,12 @@
 <div id="top-banner">
-		<a id="donate-link" href="https://creativecommons.net/donate" title="Donate to Creative Commons"></a>
+		<a id="donate-link" href="https://creativecommons.net/donate/?utm_campaign=fall2011&utm_source=ccorg&utm_medium=site_header" title="Donate to Creative Commons"></a>
         	<div class="container">
                 <div class="row">
                 <div class="eleven columns">
-		<h4><a href="https://creativecommons.net/donate">Donate to Freedom and Sharing: CC’s Annual Campaign</a></h4>
+		<h4><a href="https://creativecommons.net/donate/?utm_campaign=fall2011&utm_source=ccorg&utm_medium=site_header">Donate to Freedom and Sharing: CC’s Annual Campaign</a></h4>
 		</div>
 		<div class="three columns omega">
-                <h5><a href="https://creativecommons.net/donate" class="btn">Donate now</a></h5>
+                <h5><a href="https://creativecommons.net/donate/?utm_campaign=fall2011&utm_source=ccorg&utm_medium=site_header" class="btn">Donate now</a></h5>
                 </div>
                 </div>
                 </div>
