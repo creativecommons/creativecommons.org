2e575d0587c559fadc73ddeecd1ee7d87af1ad17
cxadams <cxadams@committer.creativecommons.org>
2011-10-24 03:26:18 +0000

    switch dropdown menus to use standard bootstrap plugin

--- docroot/wp-content/themes/creativecommons.org/js/script.js
+++ docroot/wp-content/themes/creativecommons.org/js/script.js
@@ -11,15 +11,7 @@ $(document).ready(function(){
   // Dropdown for topbar nav
   // ===============================
 
-  $("body").bind("click", function (e) {
-    $('.dropdown-toggle, .menu').parent("li").removeClass("open");
-  });
-
-  $(".dropdown-toggle, .menu").click(function (e) {
-    $('.dropdown-toggle').not(this).parent("li").removeClass("open");
-    var $li = $(this).parent('li').toggleClass('open');
-    return false;
-  });
+  $('.topbar').dropdown();
   
   // Help popovers
   // ===============================
