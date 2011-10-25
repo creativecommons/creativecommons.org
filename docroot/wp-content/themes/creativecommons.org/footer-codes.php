d71c4843d640a83ca88c9dfe94e5bcab701d0bfa
rejon <rejon@committer.creativecommons.org>
2011-10-25 17:06:14 +0000

    updates

--- docroot/wp-content/themes/creativecommons.org/footer-codes.php
+++ docroot/wp-content/themes/creativecommons.org/footer-codes.php
@@ -1,3 +1,7 @@
+	<!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="<?php bloginfo('stylesheet_directory'); ?>/js/libs/jquery-1.6.2.min.js"><\/script>')</script>
+
 	<!-- scripts concatenated and minified via build script -->
 	<script defer src="<?php bloginfo('stylesheet_directory'); ?>/js/plugins.js"></script>
 	<script defer src="<?php bloginfo('stylesheet_directory'); ?>/js/script.js"></script>
--- docroot/wp-content/themes/creativecommons.org/footer.php
+++ docroot/wp-content/themes/creativecommons.org/footer.php
@@ -1,10 +1,6 @@
         <?php include 'page-footer.php'; ?>
 	</div> <!--! end of #container -->
 
-	<!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
-	<script>window.jQuery || document.write('<script src="<?php bloginfo('stylesheet_directory'); ?>/js/libs/jquery-1.6.2.min.js"><\/script>')</script>
-
         <?php include 'footer-codes.php'; ?>
     <script>
 		var _gaq=[['_setAccount','UA-2010376-1'],['_trackPageview'],['_trackPageLoadTime']];
