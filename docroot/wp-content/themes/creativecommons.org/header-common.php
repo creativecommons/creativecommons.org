877470fc78c5712de0df33fa4598c56e9d242011
rejon <rejon@committer.creativecommons.org>
2011-10-31 09:50:58 +0000

    omg you can't believe how bad that bug was

--- docroot/wp-content/themes/creativecommons.org/header-common.php
+++ docroot/wp-content/themes/creativecommons.org/header-common.php
@@ -11,7 +11,7 @@
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/css/ie8-and-down.css">
 	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
-    <script src="<?php bloginfo('stylesheet_directory'); ?>/js/libs/modernizr-1.0.6.min.js"></script>
+    <script src="<?php bloginfo('stylesheet_directory'); ?>/js/libs/modernizr-2.0.6.min.js"></script>
 
     <meta name="keywords" content="creative commons, commons, free culture, open source, attribution, non-commercial, share-alike, no derivatives, lessig, sharing, sharism" />
     <meta name="description" content="Creative Commons licenses provide a flexible range of protections and freedoms for authors, artists, and educators. <?php if ( function_exists(is_single) && function_exists(cc_post_excerpt) && is_single()) echo cc_post_excerpt() ?>" />
