b0063d3604fdf3fb884b7280d4a81e5437320e5c
nkinkade <nkinkade@committer.creativecommons.org>
2011-11-04 20:44:22 +0000

    Switch Analytics source based on http host.

--- docroot/wp-content/themes/creativecommons.org/banner-donate.php
+++ docroot/wp-content/themes/creativecommons.org/banner-donate.php
@@ -6,7 +6,26 @@
 		<h4><a href="https://creativecommons.net/donate/?utm_campaign=fall2011&amp;utm_source=ccorg&amp;utm_medium=site_header">Donate to Freedom and Sharing: CC’s Annual Campaign</a></h4>
 		</div>
 		<div class="three columns omega">
-                <h5><a href="https://creativecommons.net/donate/?utm_campaign=fall2011&amp;utm_source=ccorg&amp;utm_medium=site_header" class="btn">Donate now</a></h5>
+<?php
+
+	// We need to switch the Analytics source based on which site this banner 
+	// shows up on.
+	switch ($_SERVER['HTTP_HOST']) {
+		case 'wiki.creativecommons.org': 
+			$analytics_source = 'ccwiki';
+			break;
+		case 'search.creativecommons.org':
+			$analytics_source = 'ccsearch';
+			break;
+		case 'labs.creativecommons.org':
+			$analytics_source = 'cclabs';
+			break;
+		default:
+			$analytics_source = 'ccorg';
+	}
+	echo "            <h5><a href='https://creativecommons.net/donate/?utm_campaign=fall2011&amp;utm_source={$analytics_source}&amp;utm_medium=site_header' class='btn'>Donate now</a></h5>";
+
+?>
                 </div>
                 </div>
                 </div>
