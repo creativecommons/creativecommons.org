a02d99ba418b393f88315368344c4c2a4a18125c
Christopher Allan Webber <cwebber@dustycloud.org>
2012-01-13 16:04:27 -0600

    Also fix gettext calls in xmp template module

--- python_env/src/cc.engine/cc/engine/chooser/xmp_template.py
+++ python_env/src/cc.engine/cc/engine/chooser/xmp_template.py
@@ -1,4 +1,3 @@
-import string
 import re
 
 from cc.i18n.gettext_i18n import ugettext_for_locale
@@ -58,15 +57,14 @@ def get_xmp_info(request_form, license, locale):
             notice = ""
 
         i18n_work = ugettext('work')
-        work_notice_template = string.Template(
-            ugettext(
+        work_notice_template = ugettext(
                 u'This %(work_type)s is licensed under a '
                 u'<a rel="license" href="%(license_url)s">Creative Commons '
-                u'%(license_name)s License</a>.'))
-        work_notice = work_notice_template.substitute(
-            {'license_name': license.title(locale_to_lower_lower(locale)),
-             'license_url': license.uri,
-             'work_type': i18n_work})
+                u'%(license_name)s License</a>.')
+        work_notice = work_notice_template % {
+            'license_name': license.title(locale_to_lower_lower(locale)),
+            'license_url': license.uri,
+            'work_type': i18n_work}
 
         notice = notice + work_notice
 
