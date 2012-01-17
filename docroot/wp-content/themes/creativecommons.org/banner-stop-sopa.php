acca328b723dc58903ce36d28af64f9546ce0f3a
nkinkade <nkinkade@committer.creativecommons.org>
2012-01-17 21:54:14 +0000

    Some changes to the colors of links in the Stop SOPA banner.

--- docroot/wp-content/themes/creativecommons.org/banner-stop-sopa.php
+++ docroot/wp-content/themes/creativecommons.org/banner-stop-sopa.php
@@ -1,11 +1,9 @@
-<div id="stop-sopa-banner" style="background-color: black; color: white; font-weight: bold; font-size: x-large; padding: 2em;">
+<div id="stop-sopa-banner" style="background-color: black; color: white; font-weight: bold; font-size: 170%; padding: 1.5em;">
 	<div class="container">
 		<div class="row">
 			<div class="sixteen columns">
-				Stop SOPA, the Internet Blacklist bill.
-				<a href="https://www.eff.org/deeplinks/2011/12/fight-blacklist-toolkit-anti-sopa-activists">12
-things you can do <stron>NOW</strong></a>.
-				<a href="https://creativecommons.org/weblog/entry/30836">Read more</a>.
+				<div>Help the Internet remain free: <a href="http://wikimediafoundation.org/wiki/English_Wikipedia_anti-SOPA_blackout" style="color: yellow;">Join Wikipedia</a> and other orgs to <a href="http://americancensorship.org" style="color: yellow;">stop SOPA/PIPA</a>.</div>
+				<div style="margin-top: 1em;">Help public research remain free:  <a href="http://www.thepetitionsite.com/207/support-the-open-access-movement-stop-the-research-works-act/?cid=FB_TAF" style="color: blue;">Sign the petition</a> to <a href="http://www.taxpayeraccess.org/action/action_access/12-0106.shtml" style="color: blue;">stop the Research Works Act</a>.</div>
 			</div>
 		</div>
 	</div>
