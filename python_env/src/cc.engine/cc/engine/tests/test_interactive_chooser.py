8c4d9810503244150092e400d4135a52514b8228
Jonathan Palecek <jonathan@creativecommons.org>
2012-04-17 13:53:52 -0500

    updated interactive chooser tests

--- python_env/src/cc.engine/cc/engine/tests/test_interactive_chooser.py
+++ python_env/src/cc.engine/cc/engine/tests/test_interactive_chooser.py
@@ -1,8 +1,29 @@
-import subprocess
-from test_js import jstest, JavascriptError
+#from paste.deploy import loadapp
+#from flup.server.fcgi import WSGIServer
+from test_js import jstest
 
-PASTER = subprocess.Popen(["paster", "serve", "cc.engine.ini"])
+
+#def launch_test_server(foo):
+#    import os, time
+#    WSGIServer(loadapp('config:' + os.getcwd()+"/cc.engine.ini")).run()
+#    time.sleep(20)
+#    foo()
 
 
-def test_something():
-    jstest("http://localhost:6543/", "ASSERT(false, 'test should fail');")
+def test_current_state_initialization():
+    #jstest("http://localhost:6543/", "ASSERT(false, 'test should fail');")
+    jstest("http://localhost:6543/choose/", 
+           """
+           WAITFOR("window.$ !== undefined && window.CHOOSER !== undefined", 
+                   function () {
+
+              WAITFOR("CHOOSER.STATE.current.field_format !== undefined",
+                      function () {
+
+                  ASSERT(CHOOSER.STATE.current.field_format == "",
+                         'arbitrary assertion');
+              });
+           });
+           """,
+           ignore=["TypeError: 'undefined' is not a function"])
+
