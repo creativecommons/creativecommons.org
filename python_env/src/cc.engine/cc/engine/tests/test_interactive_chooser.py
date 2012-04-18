e82d1996a5acd357b90123db4cec542a4f97c24f
Jonathan Palecek <jonathan@creativecommons.org>
2012-04-18 13:20:26 -0500

    cleanup

--- python_env/src/cc.engine/cc/engine/tests/test_interactive_chooser.py
+++ python_env/src/cc.engine/cc/engine/tests/test_interactive_chooser.py
@@ -1,28 +1,17 @@
-#from paste.deploy import loadapp
-#from flup.server.fcgi import WSGIServer
 from test_js import jstest
 
 
-#def launch_test_server(foo):
-#    import os, time
-#    WSGIServer(loadapp('config:' + os.getcwd()+"/cc.engine.ini")).run()
-#    time.sleep(20)
-#    foo()
 
 
 def test_current_state_initialization():
-    #jstest("http://localhost:6543/", "ASSERT(false, 'test should fail');")
     jstest("http://localhost:6543/choose/", 
            """
            WAITFOR("window.$ !== undefined && window.CHOOSER !== undefined", 
                    function () {
 
-              WAITFOR("CHOOSER.STATE.current.field_format !== undefined",
-                      function () {
-
-                  ASSERT(CHOOSER.STATE.current.field_format == "",
-                         'arbitrary assertion');
-              });
+              VIGIL("CHOOSER.STATE.current.field_format !== undefined",
+                    "CHOOSER.STATE.current.field_format == ''",
+                    "verify that CHOOSER.STATE.current is populated after page load.");
            });
            """,
            ignore=["TypeError: 'undefined' is not a function"])
