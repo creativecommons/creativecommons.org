98572703dd05ffd6d3307b07b5ea6b7d53960704
nkinkade <nkinkade@committer.creativecommons.org>
2012-06-20 19:44:05 +0000

    Removed all bloginfo() refs and replaced with static, absolute URLs to fix breakage on labs.

--- docroot/wp-content/themes/creativecommons.org/page-nav.php
+++ docroot/wp-content/themes/creativecommons.org/page-nav.php
@@ -9,42 +9,42 @@
 						<li class="dropdown">
 							<a href="#about" class="dropdown-toggle">Menu</a>
 								<ul class="menu-dropdown">
-									<li><a href="<?php bloginfo('home')?>/about">About</a></li>
-									<li><a href="<?php bloginfo('home')?>/licenses/">Licenses</a></li>
-									<li><a href="<?php bloginfo('home')?>/publicdomain/">Public Domain</a></li>
+									<li><a href="http://creativecommons.org/about">About</a></li>
+									<li><a href="http://creativecommons.org/licenses/">Licenses</a></li>
+									<li><a href="http://creativecommons.org/publicdomain/">Public Domain</a></li>
 									<li><a href="https://creativecommons.net/donate/">Support CC</a></li>
-									<li><a href="<?php bloginfo('home')?>/culture">Projects</a></li>
-									<li><a href="<?php bloginfo('home')?>/weblog">News</a></li>
+									<li><a href="http://creativecommons.org/culture">Projects</a></li>
+									<li><a href="http://creativecommons.org/weblog">News</a></li>
 								</ul>
 						</li>
 					</ul>
 					<ul id="wide-menu" class="nav">
 				    <li class="dropdown">
-				    	<a href="<?php bloginfo('home')?>/about" class="dropdown-toggle">About</a>
+				    	<a href="http://creativecommons.org/about" class="dropdown-toggle">About</a>
 				    	<ul class="menu-dropdown">
-						    <li><a href="<?php bloginfo('home')?>/about">About CC</a></li>
-						    <li><a href="<?php bloginfo('home')?>/about/history">History</a></li>
+						    <li><a href="http://creativecommons.org/about">About CC</a></li>
+						    <li><a href="http://creativecommons.org/about/history">History</a></li>
 						    <li class="divider"></li>
-						    <li><a href="<?php bloginfo('home')?>/who-uses-cc">Who Uses CC?</a></li>
+						    <li><a href="http://creativecommons.org/who-uses-cc">Who Uses CC?</a></li>
 						    <li><a href="http://wiki.creativecommons.org/Case_Studies">Case Studies</a></li>
-						    <li><a href="<?php bloginfo('home')?>/videos/">Videos about CC</a></li>
+						    <li><a href="http://creativecommons.org/videos/">Videos about CC</a></li>
 						    <li class="divider"></li>
 						    <li><strong><span>The Team</span></strong></li>
-						    <li><a href="<?php bloginfo('home')?>/board">Board of Directors</a></li>
-						    <li><a href="<?php bloginfo('home')?>/staff">Staff</a></li>
+						    <li><a href="http://creativecommons.org/board">Board of Directors</a></li>
+						    <li><a href="http://creativecommons.org/staff">Staff</a></li>
 						    <li><a href="http://wiki.creativecommons.org/CC_Affiliate_Network">Affiliate Network</a></li>
-						    <li><a href="<?php bloginfo('home')?>/opportunities">Job Opportunities</a></li>
+						    <li><a href="http://creativecommons.org/opportunities">Job Opportunities</a></li>
 						    <li class="divider"></li>
 						    <li><a href="http://wiki.creativecommons.org/FAQ">Frequently Asked Questions</a></li>
-						    <li><a href="<?php bloginfo('home')?>/contact">Contact Us</a></li>
+						    <li><a href="http://creativecommons.org/contact">Contact Us</a></li>
 						    
 				      </ul>
 				    </li>
 				    <li class="dropdown">
-				    	<a href="<?php bloginfo('home')?>/licenses/" class="dropdown-toggle">Licenses</a>
+				    	<a href="http://creativecommons.org/licenses/" class="dropdown-toggle">Licenses</a>
 				    	<ul class="menu-dropdown">
-		    <li><a href="<?php bloginfo('home')?>/licenses/">About the Licenses</a></li>
-		    <li><a href="<?php bloginfo('home')?>/choose/">Choose a License</a></li>
+		    <li><a href="http://creativecommons.org/licenses/">About the Licenses</a></li>
+		    <li><a href="http://creativecommons.org/choose/">Choose a License</a></li>
 		    <li><a href="http://search.creativecommons.org">Find licensed content</a></li>
 						    <li class="divider"></li>
 		    <li><span>Licensors</span></li>
@@ -64,15 +64,15 @@
 				      </ul>
 				    </li>
 				    <li class="dropdown">
-				    	<a href="<?php bloginfo('home')?>/publicdomain/" class="dropdown-toggle">Public Domain</a>
+				    	<a href="http://creativecommons.org/publicdomain/" class="dropdown-toggle">Public Domain</a>
 				    	<ul class="menu-dropdown">
-					<li><a href="<?php bloginfo('home')?>/publicdomain/">Our Public Domain Tools</a></li>	
+					<li><a href="http://creativecommons.org/publicdomain/">Our Public Domain Tools</a></li>	
 					<li class="divider"></li>
-						    <li><a href="<?php bloginfo('home')?>/about/cc0">About CC0 Public Domain Dedication</a></li>
-						    <li><a href="<?php bloginfo('home')?>/choose/zero/">Choose CC0 Public Domain Dedication </a></li>
+						    <li><a href="http://creativecommons.org/about/cc0">About CC0 Public Domain Dedication</a></li>
+						    <li><a href="http://creativecommons.org/choose/zero/">Choose CC0 Public Domain Dedication </a></li>
 						    <li class="divider"></li>
-						    <li><a href="<?php bloginfo('home')?>/about/pdm">About Public Domain Mark</a></li>
-						    <li><a href="<?php bloginfo('home')?>/choose/mark/">Use Public Domain Mark</a></li>
+						    <li><a href="http://creativecommons.org/about/pdm">About Public Domain Mark</a></li>
+						    <li><a href="http://creativecommons.org/choose/mark/">Use Public Domain Mark</a></li>
 						 </ul>
 				    </li>
 				    
@@ -91,35 +91,35 @@
 				    </li>
 				    
 				    <li class="dropdown">
-				    	<a href="<?php bloginfo('home')?>/culture" class="dropdown-toggle">Projects</a>
+				    	<a href="http://creativecommons.org/culture" class="dropdown-toggle">Projects</a>
 				    	<ul class="menu-dropdown">
-						    <li><a href="<?php bloginfo('home')?>/culture">Culture</a></li>
-						    <li><a href="<?php bloginfo('home')?>/education">Education</a></li>
-						    <li><a href="<?php bloginfo('home')?>/science">Science</a></li>
-						    <li><a href="<?php bloginfo('home')?>/government">Government</a></li>
+						    <li><a href="http://creativecommons.org/culture">Culture</a></li>
+						    <li><a href="http://creativecommons.org/education">Education</a></li>
+						    <li><a href="http://creativecommons.org/science">Science</a></li>
+						    <li><a href="http://creativecommons.org/government">Government</a></li>
 						    <li class="divider"></li>
                             <li><a href="http://wiki.creativecommons.org/4.0">Versioning the license suite</a></li>
 						    <li><a href="http://wiki.creativecommons.org/LRMI">Learning Resource Metadata Initiative (LRMI) </a></li>
-						    <li><a href="<?php bloginfo('home')?>/taa-grant-program">Gates OPEN</a></li>
-						    <li><a href="<?php bloginfo('home')?>/music-communities">Music Communities</a></li>
-                            <li><a href="<?php bloginfo('home')?>/record-labels">Record Labels</a></li>
+						    <li><a href="http://creativecommons.org/taa-grant-program">Gates OPEN</a></li>
+						    <li><a href="http://creativecommons.org/music-communities">Music Communities</a></li>
+                            <li><a href="http://creativecommons.org/record-labels">Record Labels</a></li>
 				      </ul>
 				    </li>
 				    <li class="dropdown">
-				    <a href="<?php bloginfo('home')?>/weblog" class="dropdown-toggle">News</a>
+				    <a href="http://creativecommons.org/weblog" class="dropdown-toggle">News</a>
 				    	<ul class="menu-dropdown">
-						    <li><a href="<?php bloginfo('home')?>/weblog">Blog</a></li>
+						    <li><a href="http://creativecommons.org/weblog">Blog</a></li>
 						    <li><a href="http://planet.creativecommons.org/">Affiliate News</a></li>
 						    <li class="divider"></li>
-						    <li><a href="<?php bloginfo('home')?>/interviews">Interviews</a></li>
+						    <li><a href="http://creativecommons.org/interviews">Interviews</a></li>
 						    <li><a href="http://wiki.creativecommons.org/Events">Events</a></li>
 						    <li><a href="http://creativecommons.org/newsletter">Newsletters</a></li>
 						    <li class="divider"></li>
-						    <li><a href="<?php bloginfo('home')?>/about/press">Press Room</a></li>
+						    <li><a href="http://creativecommons.org/about/press">Press Room</a></li>
 						 </ul>
 					</li>
 				</ul>
-				<form action="<?php bloginfo('home')?>" id="search_form">
+				<form action="http://creativecommons.org" id="search_form">
 					<input type="hidden" name="stype" value="site" id="find_site" />
 					<input type="text" name="q" placeholder="Site Search" />
 					<input type="submit" id="glass" title="Search" value="Submit">
