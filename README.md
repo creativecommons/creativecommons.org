f9bb75fa85d70b9d67c883f806af590381604f5d
Dan Mills <code@sandmill.org>
2013-11-04 02:27:05 +0000

    add python env and components; apache conf; server setup script

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # CC WordPress 2013
+
+<!--
+<VirtualHost *:8080>
+    Use CCVHost staging.creativecommons.org http
+</VirtualHost>
+
+<VirtualHost *:443>
+    Use CCVHost staging.creativecommons.org https
+    SSLEngine on
+    SSLCertificateFile /etc/ssl/private/staging.creativecommons.org.crt
+    SSLCertificateKeyFile /etc/ssl/private/staging.creativecommons.org.key
+    SSLCACertificateFile /etc/ssl/certs/RapidSSL_CA_bundle.pem 
+</VirtualHost>
+-->
---
+++ server_bootstrap.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+CUR=`cwd`
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd ${DIR}
+
+git submodule init
+git submodule update
+
+cd python_env
+
+sed -e "s|@env_dir@|${DIR}/python_env|" < bin/ccengine.fcgi.in > bin/ccengine.fcgi
+chmod 755 bin/ccengine.fcgi
+
+virtualenv .
+source bin/activate
+
+for i in 'setuptools>=0.7' 'zope.interface>=3.8.0' Paste PasteDeploy PasteScript
+do
+    pip install $i
+done
+
+cd src
+
+for i in i18n license.rdf cc.license cc.engine
+do
+    cd $i
+    python bootstrap.py -v 2.1.1
+    bin/buildout
+    python setup.py develop
+    cd ..
+done
+
+cd i18n
+    bin/compile_mo
+    bin/transstats
+cd ../.. # python_env
+
+cd ${CUR}
