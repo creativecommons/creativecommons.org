c0904aa6f8fae832cef7add94fec2f18ee93bcf4
Dan Mills <code@sandmill.org>
2013-11-06 12:55:03 -0800

    update i18n submodule; add an update script

---
+++ scripts/update.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+CWD=`pwd`
+TOPDIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd)"
+cd "${TOPDIR}"
+
+# Update toplevel repository and submodules
+
+git pull
+git submodule update
+
+# Update l10n strings in i18n submodule and sync with Transifex/GitHub
+
+cd python_env/src/i18n/
+
+git checkout master # make sure we're on a branch, git submodules have
+                    # a bad habit of having a detached HEAD
+
+scripts/runcheckouts.sh
+scripts/extract.sh
+
+tx push -s
+tx pull -a --mode=reviewed
+
+git commit -m "New strings extracted from sources" cc/i18n/po/en/cc_org.po
+git commit -a -m "Latest i18n updates from Transifex"
+git push
+
+cd "${TOPDIR}"
+
+# Update toplevel repository to point to latest i18n rev
+
+git commit -m "Update i18n submodule with latest strings/translations" python_env/src/i18n
+
+cd "${CWD}"
