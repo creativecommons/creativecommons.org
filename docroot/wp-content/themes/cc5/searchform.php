10257e4ab99d33f5affe30ff193fd01886804ce2
mzeinstra <mzeinstra@MacBook-Air-Maarten.local>
2013-12-04 22:20:06 +0100

    Removed old Themes
    These should not be in the current server anymore.
    
    Could be published as separate repositories

--- docroot/wp-content/themes/cc5/searchform.php
+++
@@ -1,6 +0,0 @@
-<form method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
-<div>
-<input type="text" value="<?php the_search_query(); ?>" name="s" id="s" size="30" />
-<input type="submit" id="searchsubmit" value="Search" />
-</div>
-</form>
