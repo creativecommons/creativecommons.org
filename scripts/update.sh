de0ab83bc1042f96c4337ce90250cbec6aff8fbb
Dan Mills <code@sandmill.org>
2014-02-06 22:02:14 +0000

    implement rudimentary branch-switching functionality via ~/server-config files (maintained in a separate repo)

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,8 +1,35 @@
 #!/bin/bash
 
 CWD=`pwd`
-TOPDIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd)"
+NAME=$( dirname "${BASH_SOURCE[0]}")
+TOPDIR="$(cd "${NAME}/.." && pwd)"
 cd "${TOPDIR}"
+
+# This pretty hacky - should have update script be part of
+# server-config repo, perhaps?
+#
+# But regardless - if ~/server-config exists and it has a file for our
+# server name (assuming our dirname is our server name...), then
+# assume it has some ini-style settings for us. In particular, the
+# 'branch' setting will tell us which branch we should switch to.
+
+# Find out if we should be using something other than the master
+# branch (the default)
+
+BRANCH=master
+if [[ -d "~/server-config" && -f "~/server-config/${NAME}" ]]; then
+    BRANCH=`grep ^branch "~/server-config/${NAME}" | sed -e 's/^branch\s*=\s*//'`
+fi
+
+# Get requested branch from origin if this is the first time
+
+if [[ -z `git branch | grep ${BRANCH}` ]]; then
+    git checkout -b ${BRANCH} origin/${BRANCH}
+fi
+
+# In case we hadn't already, make sure to switch to the requested branch
+
+git checkout ${BRANCH}
 
 # Update toplevel repository and submodules
 
@@ -45,6 +72,8 @@ fi
 # In case translation files changed:
 python_env/bin/compile_mo
 python_env/bin/transstats
+python_env/src/i18n/bin/compile_mo
+python_env/src/i18n/bin/transstats
 
 # Eek - how do we figure out if a restart is needed?
 sudo /etc/init.d/apache2 restart
