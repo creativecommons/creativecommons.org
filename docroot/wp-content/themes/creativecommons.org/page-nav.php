ec38c6ad9ba0b4bd6e52aabf4b3055c7733cc135
Matt Lee <mattl@creativecommons.org>
2014-05-14 13:56:36 -0400

    don't force HTTP navigation

--- docroot/wp-content/themes/creativecommons.org/page-nav.php
+++ docroot/wp-content/themes/creativecommons.org/page-nav.php
@@ -9,42 +9,42 @@
 						<li class="dropdown">
 							<a href="#about" class="dropdown-toggle">Menu</a>
 								<ul class="menu-dropdown">
-									<li><a href="http://creativecommons.org/about">About</a></li>
-									<li><a href="http://creativecommons.org/licenses/">Licenses</a></li>
-									<li><a href="http://creativecommons.org/publicdomain/">Public Domain</a></li>
-									<li><a href="https://creativecommons.net/donate/">Support CC</a></li>
-									<li><a href="http://creativecommons.org/culture">Projects</a></li>
-									<li><a href="http://creativecommons.org/weblog">News</a></li>
+									<li><a href="/about">About</a></li>
+									<li><a href="/licenses/">Licenses</a></li>
+									<li><a href="/publicdomain/">Public Domain</a></li>
+									<li><a href="https://donate.creativecommons.org">Support CC</a></li>
+									<li><a href="/culture">Projects</a></li>
+									<li><a href="/weblog">News</a></li>
 								</ul>
 						</li>
 					</ul>
 					<ul id="wide-menu" class="nav">
 				    <li class="dropdown">
-				    	<a href="http://creativecommons.org/about" class="dropdown-toggle">About</a>
+				    	<a href="/about" class="dropdown-toggle">About</a>
 				    	<ul class="menu-dropdown">
-						    <li><a href="http://creativecommons.org/about">About CC</a></li>
-						    <li><a href="http://creativecommons.org/about/history">History</a></li>
+						    <li><a href="/about">About CC</a></li>
+						    <li><a href="/about/history">History</a></li>
 						    <li class="divider"></li>
-						    <li><a href="http://creativecommons.org/who-uses-cc">Who Uses CC?</a></li>
+						    <li><a href="/who-uses-cc">Who Uses CC?</a></li>
 						    <li><a href="http://wiki.creativecommons.org/Case_Studies">Case Studies</a></li>
-						    <li><a href="http://creativecommons.org/videos/">Videos about CC</a></li>
+						    <li><a href="/videos/">Videos about CC</a></li>
 						    <li class="divider"></li>
 						    <li><strong><span>The Team</span></strong></li>
-						    <li><a href="http://creativecommons.org/board">Board of Directors</a></li>
-						    <li><a href="http://creativecommons.org/staff">Staff</a></li>
+						    <li><a href="/board">Board of Directors</a></li>
+						    <li><a href="/staff">Staff</a></li>
 						    <li><a href="http://wiki.creativecommons.org/CC_Affiliate_Network">Affiliate Network</a></li>
-						    <li><a href="http://creativecommons.org/opportunities">Job Opportunities</a></li>
+						    <li><a href="/opportunities">Job Opportunities</a></li>
 						    <li class="divider"></li>
 						    <li><a href="http://wiki.creativecommons.org/FAQ">Frequently Asked Questions</a></li>
-						    <li><a href="http://creativecommons.org/contact">Contact Us</a></li>
+						    <li><a href="/contact">Contact Us</a></li>
 						    
 				      </ul>
 				    </li>
 				    <li class="dropdown">
-				    	<a href="http://creativecommons.org/licenses/" class="dropdown-toggle">Licenses</a>
+				    	<a href="/licenses/" class="dropdown-toggle">Licenses</a>
 				    	<ul class="menu-dropdown">
-		    <li><a href="http://creativecommons.org/licenses/">About the Licenses</a></li>
-		    <li><a href="http://creativecommons.org/choose/">Choose a License</a></li>
+		    <li><a href="/licenses/">About the Licenses</a></li>
+		    <li><a href="/choose/">Choose a License</a></li>
 		    <li><a href="http://search.creativecommons.org">Find licensed content</a></li>
 						    <li class="divider"></li>
 		    <li><span>Licensors</span></li>
@@ -64,15 +64,15 @@
 				      </ul>
 				    </li>
 				    <li class="dropdown">
-				    	<a href="http://creativecommons.org/publicdomain/" class="dropdown-toggle">Public Domain</a>
+				    	<a href="/publicdomain/" class="dropdown-toggle">Public Domain</a>
 				    	<ul class="menu-dropdown">
-					<li><a href="http://creativecommons.org/publicdomain/">Our Public Domain Tools</a></li>	
+					<li><a href="/publicdomain/">Our Public Domain Tools</a></li>	
 					<li class="divider"></li>
-						    <li><a href="http://creativecommons.org/about/cc0">About CC0 Public Domain Dedication</a></li>
-						    <li><a href="http://creativecommons.org/choose/zero/">Choose CC0 Public Domain Dedication </a></li>
+						    <li><a href="/about/cc0">About CC0 Public Domain Dedication</a></li>
+						    <li><a href="/choose/zero/">Choose CC0 Public Domain Dedication </a></li>
 						    <li class="divider"></li>
-						    <li><a href="http://creativecommons.org/about/pdm">About Public Domain Mark</a></li>
-						    <li><a href="http://creativecommons.org/choose/mark/">Use Public Domain Mark</a></li>
+						    <li><a href="/about/pdm">About Public Domain Mark</a></li>
+						    <li><a href="/choose/mark/">Use Public Domain Mark</a></li>
 						 </ul>
 				    </li>
 				    
@@ -81,39 +81,39 @@
 				    	<ul class="menu-dropdown">
 				    		<li><a href="https://creativecommons.net/">Donate to Creative Commons</a></li>
 						    <li><a href="http://store.creativecommons.org/">Buy CC Merchandise</a></li>
-				    		<li><a href="http://creativecommons.org/supporters">Our Supporters</a></li>
+				    		<li><a href="/supporters">Our Supporters</a></li>
 						    <li><a href="https://creativecommons.net/testimonials/">Testimonials</a></li>
 				    	</ul>
 				    </li>
 				    
 				    <li class="dropdown">
-				    	<a href="http://creativecommons.org/culture" class="dropdown-toggle">Projects</a>
+				    	<a href="/culture" class="dropdown-toggle">Projects</a>
 				    	<ul class="menu-dropdown">
-						    <li><a href="http://creativecommons.org/culture">Culture</a></li>
-						    <li><a href="http://creativecommons.org/education">Education</a></li>
-						    <li><a href="http://creativecommons.org/science">Science</a></li>
-						    <li><a href="http://creativecommons.org/government">Government</a></li>
+						    <li><a href="/culture">Culture</a></li>
+						    <li><a href="/education">Education</a></li>
+						    <li><a href="/science">Science</a></li>
+						    <li><a href="/government">Government</a></li>
 						    <li class="divider"></li>
-						    <li><a href="http://creativecommons.org/taa-grant-program">Gates OPEN</a></li>
+						    <li><a href="/taa-grant-program">Gates OPEN</a></li>
 						    <li><a href="http://wiki.creativecommons.org/LRMI">Learning Resource Metadata Initiative (LRMI) </a></li>
-						    <li><a href="http://creativecommons.org/music-communities">Music Communities</a></li>
+						    <li><a href="/music-communities">Music Communities</a></li>
 						    <li><a href="http://wiki.creativecommons.org/Open_Policy_Network">Open Policy Network</a></li>
-                            <li><a href="http://creativecommons.org/record-labels">Record Labels</a></li>
+                            <li><a href="/record-labels">Record Labels</a></li>
                             <li><a href="http://schoolofopen.org/">School of Open</a></li>
                             <li><a href="http://wiki.creativecommons.org/4.0">Versioning the license suite</a></li>
 				      </ul>
 				    </li>
 				    <li class="dropdown">
-				    <a href="http://creativecommons.org/weblog" class="dropdown-toggle">News</a>
+				    <a href="/weblog" class="dropdown-toggle">News</a>
 				    	<ul class="menu-dropdown">
-						    <li><a href="http://creativecommons.org/weblog">Blog</a></li>
+						    <li><a href="/weblog">Blog</a></li>
 						    <li><a href="http://planet.creativecommons.org/">Affiliate News</a></li>
 						    <li class="divider"></li>
-						    <li><a href="http://creativecommons.org/interviews">Interviews</a></li>
+						    <li><a href="/interviews">Interviews</a></li>
 						    <li><a href="http://wiki.creativecommons.org/Events">Events</a></li>
-						    <li><a href="http://creativecommons.org/newsletter">Newsletters</a></li>
+						    <li><a href="/newsletter">Newsletters</a></li>
 						    <li class="divider"></li>
-						    <li><a href="http://creativecommons.org/about/press">Press Room</a></li>
+						    <li><a href="/about/press">Press Room</a></li>
 						 </ul>
 					</li>
 				</ul>
