df6bd9bca93eee6f4bd4ca6140c2361b3fde1a82
Matt Lee <mattl@cnuk.org>
2015-05-22 14:04:35 -0400

    updated banner, removed second banner

--- docroot/wp-content/themes/creativecommons.org/banner.php
+++ docroot/wp-content/themes/creativecommons.org/banner.php
@@ -22,32 +22,9 @@
 ?>
 
 <div id="top-banner" style="background-color: transparent; ">
-
-        
-            <?php
-
-            $i = rand(1,2);
-
-if ($i == 1) {
-?>
-
+     
 	<a style="display: block;" href="https://donate.creativecommons.org/?utm_campaign=2015fund&utm_source=ccsite_header2015">
-	<img src="/images/websitebanner2015.png" style="max-height: 100%; max-width: 100%; " alt="Donate banner" />
-<?php
-}
-
-else {
-
-?>
-	<a style="display: block; " href="https://donate.creativecommons.org/?utm_campaign=2014fund&utm_source=ccorg2&utm_medium=site_header&utm_medium=site_header">
-	<img src="/wp-content/uploads/2014/11/websitebanner2014_veal.png" style="max-height: 100%; max-width: 100%; " alt="Donate banner" />
-
-<?php 
-
-}
-
-?>
-
+	<img src="/images/websitebanner2015.png" style="max-height: 100%; max-width: 100%; " alt="Keep the internet creative, free and open &mdash; Donate to Creative Commons"/>
 
 	</a>
 </div>
