e83cae6be9c9ea3585bcca2f6b59306ff0f0552c
Matt Lee <mattl@cnuk.org>
2015-06-26 12:34:55 -0400

    update for HTTPS

--- python_env/src/cc.engine/cc/engine/chooser/views.py
+++ python_env/src/cc.engine/cc/engine/chooser/views.py
@@ -27,7 +27,7 @@ CC0_HTML_FORMATTER = CC0HTMLFormatter()
 PUBLICDOMAIN_HTML_FORMATTER = PublicDomainHTMLFormatter()
 PDMARK_HTML_FORMATTER = PDMarkHTMLFormatter()
 STANDARD_SELECTOR = cc.license.LicenseSelector(
-    "http://creativecommons.org/license/")
+    "https://creativecommons.org/license/")
 
 
 def _base_context(request, target_lang=None):
@@ -224,7 +224,7 @@ def _generate_exit_url(url, referrer, license):
     return url
 
 
-NS_CC = 'http://creativecommons.org/ns#'
+NS_CC = 'https://creativeommons.org/ns#'
 NS_DC = 'http://purl.org/dc/elements/1.1/'
 NS_DCQ = 'http://purl.org/dc/terms/'
 NS_RDF = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
