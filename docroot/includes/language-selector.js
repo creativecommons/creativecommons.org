a1d6f3157c077239ef683275400d493b5334e2d0
Dale McGladdery <dale@affinitybridge.com>
2018-03-12 14:27:48 -0700

    Fix bad assumption about Deed URL language mapping

--- docroot/includes/language-selector.js
+++ docroot/includes/language-selector.js
@@ -1,14 +1,9 @@
 $( document ).ready(function() {
   $('#language-selector-block select').change(function(e) {
     var language = $(e.target).val();
-    var currentPath = window.location.pathname;
-    var newPath = currentPath;
-    if (currentPath.substr(-3, 1) == '.') {
-      newPath = currentPath.substr(0, currentPath.length -3);
-    }
-    newPath = newPath + '.' + language;
-    if (!(currentPath == '/licenses/by/4.0/deed' && newPath == '/licenses/by/4.0/deed.en')) {
-      window.location.href = newPath;
-    }
+    var deedPath = window.location.pathname.split('/');
+    deedPath.pop();
+    deedPath.push('deed.' + language);
+    window.location.href = deedPath.join('/');
   });
 });
